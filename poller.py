808ed4a3208299e85585ea13395567deaac6f4ad
Mark SKinner <m.skinner@make.com>
2024-01-10 14:17:17 +0000

    Minor bug fixes
    Logstash hostname defined incorrectly in Metricbeat environment variables
    
    Debug logging improved in Python script

--- poller.py
+++ poller.py
@@ -27,6 +27,7 @@ euUrl = "https://eu1.make.celonis.com/api/v2/admin/scenarios/logs"
 
 current_time = int(round(time.time() * 1000) - 60000)
 print("Current timestamp is: ", current_time)
+logger.debug(f"python-logstash: Logging starting at: {current_time}")
 # Define initial parameters for the first request
 parameters = {'showCheckRuns': 'true', 'pg[sortDir]': 'asc', 'from': current_time}
 
@@ -34,17 +35,17 @@ parameters = {'showCheckRuns': 'true', 'pg[sortDir]': 'asc', 'from': current_tim
 async def fetch_logs(parameters):
     global logger
 
-    print("Using imtId: ", parameters.get('pg[last]') if 'pg[last]' in parameters else "None")
+    imtIdStr = parameters.get('pg[last]') if 'pg[last]' in parameters else "None"
     connection_attempts = 0
     while connection_attempts < 5:
         try:
+            logger.debug(f"python-logstash: Making API request using imtId: {imtIdStr}")
             response = requests.get(euUrl, headers=request_headers, params=parameters)
             if response.status_code == 200:
-                print(response.url)
+                logger.debug("python-logstash: API response received")
                 break
             else:
-                print(response.reason)
-                print(response.text)
+                logger.error(f"python-logstash: Status Code ERROR: {response.text}")
                 return None
         except ConnectionRefusedError:
             # Connection is closed, wait for a short duration and retry
@@ -54,34 +55,30 @@ async def fetch_logs(parameters):
                 raise Exception("Failed to connect to Logstash after 5 attempts")
 
     json_data = response.json()
-    # print(json_data)
     # Check for scenario logs
     if not json_data['scenarioLogs']:
-        logger.warning("No Scenario Logs")
+        logger.warning("python-logstash: No Scenario Logs")
         return 'Wait'
 
     # Extract pagination information
     try:
         pg_last = json_data['scenarioLogs'][-1]["imtId"]
     except:
-        print("pg_last issue!")
+        logger.error(f"python-logstash: pg_last issue! : {str(pg_last)}")
     # Check if 'pg_last' is a string before formatting it
     if isinstance(pg_last, str):
         # Convert 'imtId' to a string if it is not already
         str_imtId = str(pg_last)
-        # print(pg_last)
         # logger.debug("Pagination - imtId for next request: %s", str_imtId)
     else:
         raise TypeError("Expected 'pg_last' to be a string, but got %s", type(pg_last))
-        logger.error("Expected 'pg_last' to be a string, but got %s", type(pg_last))
+        logger.error(f"python-logstash: Expected 'pg_last' to be a string, but got: {type(pg_last)}")
         sys.exit()
 
 
     # Process and format data
     # scenario_logs = []
     for scenario_log in json_data['scenarioLogs']:
-        # print(scenario_log)
-        # print("-----------")
         data = {
             "imtId": scenario_log['imtId'],
             "organizationId": scenario_log['organizationId'],
@@ -94,31 +91,28 @@ async def fetch_logs(parameters):
             "status": scenario_log['status'] if 'status' in scenario_log else 0
         }
         # scenario_logs.append(data)
-        # print(scenario_logs)
-        # print("_____________")
 
     # Send data to Logstash asynchronously
     # for data in scenario_logs:
         try:
-            print("Timestamp: ", data['timestamp'])
-            logger.info('python-logstash: data', extra=data)
+            logger.info(f'python-logstash: Logged data', extra=data)
             # print(data)
         except:
-            logger.error('Failed to log data: ', extra=data)
+            logger.error('python-logstash: Failed to log data', extra=data)
         # time.sleep(0.5)
     return str_imtId
 
 # Continuously fetch logs and process them
 async def main():
     pg_last = None
-    print("Logger running...")
+    print("python-logstash: Logger running...")
     while True:
         # If there is no pg_last value, make the initial request
         if pg_last is None:
             pg_last = await fetch_logs(parameters)
         elif pg_last == 'Wait':
             # Sleep for a few seconds before checking for new logs again
-            print("Sleeping for 5 seconds...")
+            # print("python-logstash: Sleeping for 5 seconds...")
             await asyncio.sleep(10)
             pg_last = await fetch_logs(parameters)
         # Use the pg_last value for subsequent requests
@@ -126,11 +120,11 @@ async def main():
             if 'from' in parameters:
                 del parameters['from']
             parameters['pg[last]'] = pg_last
-            print(f"pg[last] is: {pg_last}")
-            logger.debug(f"Pagination - imtId for next request: {parameters['pg[last]']}")
+            # print(f"python-logstash: pg[last] is: {pg_last}")
+            logger.debug(f"python-logstash: Pagination - imtId for next request: {parameters['pg[last]']}")
             pg_last = await fetch_logs(parameters)
         else:
-            print("Unknow pg_last value: ", str(pg_last))
+            logger.error(f"python-logstash: Unknow pg_last value: {str(pg_last)}")
             continue
 
 # Create an event loop
@@ -138,7 +132,6 @@ loop = asyncio.new_event_loop()
 try:
     loop.run_until_complete(main())
 except Exception as e:
-    print(e)
-    logger.exception(e)
+    # print(e)
+    logger.exception(f"python-logstash: Loop Exception: {str(e)}")
     loop.close()
-
